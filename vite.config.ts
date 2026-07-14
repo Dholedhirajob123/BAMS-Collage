25a9b7f77c0646dd524fddf8e94c6f19f31f85e6
Dholedhirajob123 <dholedhirajob123@gmail.com>
2026-07-15 03:07:12 +0530

    remove image from here

--- vite.config.ts
+++ vite.config.ts
@@ -19,26 +19,6 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
-
-    // Only increases the warning limit
     chunkSizeWarningLimit: 1000,
-
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          router: ["react-router-dom"],
-          forms: ["react-hook-form", "@hookform/resolvers", "zod"],
-          ui: [
-            "@radix-ui/react-dialog",
-            "@radix-ui/react-dropdown-menu",
-            "@radix-ui/react-tooltip",
-            "@radix-ui/react-popover",
-            "@radix-ui/react-tabs",
-          ],
-          icons: ["lucide-react"],
-        },
-      },
-    },
   },
 });
